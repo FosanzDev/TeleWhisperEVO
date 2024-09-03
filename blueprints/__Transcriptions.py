8d03d1b67490cb17c07713fbc4a638ff2d3776c9
FosanzDev <me@fosanz.dev>
2024-09-03 22:07:11 +0200

    Adding group support

--- blueprints/__Transcriptions.py
+++ blueprints/__Transcriptions.py
@@ -13,29 +13,63 @@ class __Transcriptions:
     def __init__(self, client: TelegramClient,
                  runpod_connector: RunPodConnector):
 
+        @client.on(events.NewMessage(pattern='/tw_transcribe'))
+        async def group_transcribe(event: events.NewMessage.Event):
+            if event.is_private:
+                await event.reply('Send me an audio file to transcribe!')
+            else:
+                # Get the replying message:
+                reply_message = await event.get_reply_message()
+                if reply_message is None:
+                    await event.reply('Reply and click -> /tw_transcribe to transcribe anything!')
+                else:
+                    if reply_message.media:
+                        if telethon.utils.is_audio(reply_message.media):
+                            status_message = await client.send_message(event.chat_id, parse_mode='html',
+                                                                  message='<em>Receiving file...</em>')
+                            file_name = str(uuid4()) + '.' + get_file_name(reply_message).split('.')[-1]
+                            file_path = await reply_message.download_media(file='audio/' + file_name)
+
+                            await transcribe(event, status_message, file_path)
+
+                    else:
+                        await event.reply("I can't transcribe that!")
+
+
         @client.on(events.NewMessage())
-        async def transcribe(event: events.NewMessage.Event):
+        async def private_transcribe(event: events.NewMessage.Event):
             if event.message.media:
                 if telethon.utils.is_audio(event.message.media):
-                    status_message = await client.send_message(event.message.chat_id, parse_mode='html',
-                                                            message='<em>Receiving file...</em>')
-                    file_name = str(uuid4()) + '.' + get_file_name(event.message).split('.')[-1]
-                    file_path = await event.download_media(file='audio/' + file_name)
+                    if event.is_private:
+                        status_message = await client.send_message(event.chat_id, parse_mode='html',
+                                                                    message='<em>Receiving file...</em>')
 
-                    await client.edit_message(status_message, parse_mode='html',
-                                              message='<i>Converting...</i>')
-                    mp3_filepath, file_stream = await file_manipulation.auto_to_mp3(file_path)
-                    if file_path != mp3_filepath: # If the file was not an mp3, remove the original file
-                        await file_manipulation.remove_file(file_path)
+                        file_name = str(uuid4()) + '.' + get_file_name(event.message).split('.')[-1]
+                        file_path = await event.download_media(file='audio/' + file_name)
+                        await transcribe(event, status_message, file_path)
 
-                    await client.edit_message(status_message, parse_mode='html',
-                                              message='<i>Transcribing...</i>')
-                    text = await runpod_connector.transcribe(mp3_filepath)
-                    await event.reply(text[:4095])
-                    for i in range(4095, len(text), 4095):
-                        await client.send_message(event.message.chat_id, text[i:i+4095])
+                    else:
+                        await event.reply('Reply and click -> /tw_transcribe to transcribe it!')
 
-                    await client.edit_message(status_message, parse_mode='html',
-                                              message='<b>Done!</b>')
-                    await file_manipulation.remove_file(mp3_filepath)
-                    await client.delete_messages(event.message.chat_id, [status_message.id])
+        async def transcribe(event: events.NewMessage.Event,
+                             status_message,
+                             file_path: str):
+
+            await client.edit_message(status_message, parse_mode='html',
+                                      message='<i>Converting...</i>')
+            mp3_filepath, file_stream = await file_manipulation.auto_to_mp3(file_path)
+            if file_path != mp3_filepath:
+                await file_manipulation.remove_file(file_path)
+
+            await client.edit_message(status_message, parse_mode='html',
+                                      message='<i>Transcribing...</i>')
+            text = await runpod_connector.transcribe(mp3_filepath)
+            await event.reply(text[:4095])
+            for i in range(4095, len(text), 4095):
+                await client.send_message(event.message.chat_id, text[i:i+4095])
+
+            await client.edit_message(status_message, parse_mode='html',
+                                      message='<b>Done!</b>')
+            await file_manipulation.remove_file(mp3_filepath)
+
+            await client.delete_messages(event.message.chat_id, [status_message.id])
