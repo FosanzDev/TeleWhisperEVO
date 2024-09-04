2eb9eb77319ca73407113a50b47ba7af73f1751c
FosanzDev <me@fosanz.dev>
2024-09-04 21:16:24 +0200

    Fixed mime type audio/ogg

--- blueprints/utils.py
+++ blueprints/utils.py
@@ -1,6 +1,12 @@
+from uuid import uuid4
+
 from telethon.tl.types import MessageMediaDocument, DocumentAttributeFilename
 
+
 def get_file_name(media_document: MessageMediaDocument) -> str:
+    if media_document.document.mime_type == 'audio/ogg':
+        return str(uuid4()) + '.ogg'
+
     if media_document.document:
         for attribute in media_document.document.attributes:
             if isinstance(attribute, DocumentAttributeFilename):
